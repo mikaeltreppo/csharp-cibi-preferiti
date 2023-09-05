770426b5fbc773b428bdc20fa1a15ba07e3af15e
mikaeltreppo <mikaeltreppo@outlook.com>
2023-09-05 16:35:18 +0200

    Bonus

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string[] cibi = { "pizza", "carne", "seadas", "cheesecake", "torta", "tiramisù" };
+string[] cibi = { "pizza", "carne", "seadas", "cheesecake", "torta", "tiramisù", "caffè"};
 
 //Stampo la lunghezza dell'array.
 Console.WriteLine("Gli elementi nella mia lista sono: " + cibi.Length);
@@ -17,4 +17,17 @@ Console.WriteLine($"Il mio cibo preferito è indubbiamente la {cibi[0]}");
 //Stampo il mio ultimo cibo preferito
 Console.WriteLine($"Invece uno dei cibi che più mi piacciono ma non è in cima alla classifica è il {cibi.Last()}");
 
-//Stampo tutto
+//Stampo Ciò che è in mezzo
+int mid = cibi.Length / 2;
+
+if (cibi.Length % 2 == 0)
+{
+    Console.WriteLine($"Mentre i cibi che sono nella metà della lista sono {cibi[mid]} & {cibi[mid - 1]}");
+
+}
+else
+{
+    Console.WriteLine($"Mentre un cibo che è nella metà della lista è la {cibi[mid]}");
+ 
+
+}
